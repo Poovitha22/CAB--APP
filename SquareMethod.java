66518a69e6cb44c28eb7cf2faa0ba5bb80ba214d
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-04-16 17:49:33 +0530

    Add files via upload

---
+++ SquareMethod.java
@@ -0,0 +1,16 @@
+package project1;
+
+public class SquareMethod {
+	public static int square(int a) {
+	 int b=a*a;
+	return b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9;
+		double result=square(a);
+		System.out.println(result);
+
+	}
+
+}
