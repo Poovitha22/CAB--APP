ec1b202a17ab9dbfc3b102b7cc8e53155915915f
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-04-16 17:46:39 +0530

    Add files via upload

---
+++ DisplayName.java
@@ -0,0 +1,16 @@
+package project1;
+
+public class DisplayName {
+
+	public static void poovi(String name) {
+		System.out.println("my name is " + name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "poovi";
+		poovi(name);
+
+	}
+
+}
