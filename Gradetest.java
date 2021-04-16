2421d866fffbedfdcfd71ee48ac8fe79ec4032c8
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-04-16 17:47:04 +0530

    Add files via upload

---
+++ Gradetest.java
@@ -0,0 +1,16 @@
+package project1;
+
+public class Gradetest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=95;
+if(n>90)
+System.out.println("Grade A");
+else if(n>50)
+System.out.println("Grade B");
+else 
+	System.out.println("Grade C");
+	}
+
+}
