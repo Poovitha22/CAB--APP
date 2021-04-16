a0db610bfd5b7e6fb8cfe3b7d61e08aa8387dbe8
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-04-16 17:45:36 +0530

    Add files via upload

---
+++ AddTwo.java
@@ -0,0 +1,22 @@
+package project1;
+
+public class AddTwo {
+
+	public static int add(int a,int b) {
+		// TODO Auto-generated constructor stub
+		int c=a+b;
+		return c;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODinO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int x=add(a,b);
+		System.out.println("Sum of two numbers "+x);
+
+       
+	}
+
+}
