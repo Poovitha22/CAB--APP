db62fcae14ca9d9ad99f2d77ec44647d64a450b3
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-04-16 17:47:35 +0530

    Add files via upload

---
+++ MathSquare.java
@@ -0,0 +1,13 @@
+package project1;
+
+public class MathSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=9; 
+		double result=Math.sqrt(n);
+		System.out.println(result);
+
+	}
+
+}
