03793be157384d27245370d0088c8f6e4630eac1
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-04-19 17:22:14 +0530

    Add files via upload

---
+++ Testvalidation.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class Testvalidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int marks=10;
+if(marks>0)
+	System.out.println("Valid");
+else
+	System.out.println("Invalid");
+	}
+
+}
