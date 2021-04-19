e8cecd528d044f23db0dba2157f63db60278a696
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-04-19 17:21:56 +0530

    Add files via upload

---
+++ Teststring.java
@@ -0,0 +1,13 @@
+package project1;
+
+public class Teststring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String firstname="poovitha";
+String lastname="thangamayan";
+String fullname=(firstname+lastname);
+System.out.println(fullname);
+	}
+
+}
